377aed859329ee3fef5d48b5083f01cde85a4aed
Lars Backman <lars@backman.fyi>
2022-06-11 22:45:39 +0200

    Update backup script to put file into folder

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,7 +2,7 @@
 
 backed_up_archive="$(date --iso-8601=hour).tar.gz"
 
-tar --force-local -czvf $backed_up_archive lanbros_survival/lanbros_survival lanbros_survival/level.dat
-aws s3 cp $backed_up_archive "s3://lanbros-mc-backup/$(hostname)/"
+tar --force-local -czvf $backed_up_archive server_root/lanbros_survival
+/usr/local/aws-cli/v2/current/bin/aws s3 cp $backed_up_archive "s3://lanbros-mc-backup/$(hostname)/"
 
 rm -f $backed_up_archive
